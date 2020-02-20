c5bdf1d0cc0e8fbdd1f67dd7da5986fce9ac753a
Mitzi Estrada <m_estrada17@live.com>
2020-02-20 06:08:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  dwarves.each_with_index { |dwarf, index|
+  print "#{index + 1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
+#def summon_captain_planet(array)# code an argument here
   # Your code here
-end
+  array.map do |str|
+    str.capitalize + '!'
+   end
+#end
 
-def long_planeteer_calls# code an argument here
+#def long_planeteer_calls(array)# code an argument here
   # Your code here
-end
+  array.any? { |element| element.length > 4}
+#end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+long_planeteer_calls(array)
+
+#def find_the_cheese(arr)
+  #cheese_types = ["cheddar", "gouda", "camembert"]
+#arr.find do |str|
+    #cheese_types.include?(str)
+#end
+#end
+
+
+
+
