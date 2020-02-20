fb06906c9491481c5721ff018d2d561a49ea5478
Mitzi Estrada <m_estrada17@live.com>
2020-02-20 06:14:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  array.any? { |element| element.length > 4}
+  array.any? { |el| el.length > 4}
 end
 
 long_planeteer_calls(array)
