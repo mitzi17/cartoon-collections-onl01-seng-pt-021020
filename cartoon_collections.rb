f7142b6659bc0e722a94c0b208435e974f69f86b
Mitzi Estrada <m_estrada17@live.com>
2020-02-20 06:09:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,8 @@
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
-  dwarves.each_with_index { |dwarf, index|
+  dwarves.each_with_index do { |dwarf, index|
   print "#{index + 1}. #{dwarf}"}
+end
 end
 
 def summon_captain_planet(array)# code an argument here
