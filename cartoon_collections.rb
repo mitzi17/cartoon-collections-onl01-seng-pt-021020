aa8e94de5ac53931b49d57d85d1892d6564c11ad
Mitzi Estrada <m_estrada17@live.com>
2020-02-20 06:14:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls(array)# code an argument here
   array.any? { |el| el.length > 4}
 end
 
-long_planeteer_calls(array)
+
 
 def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
